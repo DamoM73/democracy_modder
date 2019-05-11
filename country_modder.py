03a73e352ce00a220f4d9d36fd9c32fffae9e5ea
DamoM73 <damomurtagh@gmail.com>
2019-05-11 20:26:54 +1000

    Part 2

--- country_modder.py
+++ country_modder.py
@@ -13,20 +13,26 @@ root.config(bg="white")
 
 # **** Add content to window ****
 
-# header frame
-header_fr = tk.Frame(root, bg="white", width=WIDTH).grid(row=0, column=0) 
+# ** header frame
+head_fr = tk.Frame(root, bg="white", width=WIDTH).grid(row=0, column=0) 
 logo = tk.PhotoImage(file="logo_small.png")
-tk.Label(header_fr, image= logo, bg="white").grid(row=0, column=0, sticky=tk.W)
-tk.Label(header_fr, text="Country Generator ", font=("Arial", 22), justify=tk.RIGHT, bg="white")\
+tk.Label(head_fr, image= logo, bg="white").grid(row=0, column=0, sticky=tk.W)
+tk.Label(head_fr, text="Country Generator ", font=("Arial", 22), justify=tk.RIGHT, bg="white")\
     .grid(row=0, column=1, sticky=tk.E)
 
-# country details frame
+# ** country details frame
 country_fr = tk.LabelFrame(root, text="Country Details", font=("Arial",16), bg="white")
 country_fr.grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
-tk.Label(country_fr,text="Name", bg="white", font=("Arial",12)).grid(row=0, column=0)
-name_ent = tk.Entry(country_fr, width=30)
-name_ent.grid(row=0, column=1)
 
+# country name
+tk.Label(country_fr,text="Name", bg="white", font=("Arial",12), width=15, justify=tk.LEFT)\
+    .grid(row=0, column=0, sticky=tk.W)
+name_ent = tk.Entry(country_fr, width=30)
+name_ent.grid(row=0, column=1, padx=5, pady=5)
+
+# leader title
+tk.Label(country_fr,text="Leader Title", bg="white", font=("Arial", 12), width=15, justify=tk.LEFT)\
+    .grid(row=1, column=0, stick=tk.W)
 
 # **** run window loop ****
 root.mainloop()
