95d2165e4e09936c0995a23d435f9f6847a3093f
DamoM73 <damomurtagh@gmail.com>
2019-06-05 18:38:03 +1000

    Class modder start

---
+++ country modder class v1.py
@@ -0,0 +1,20 @@
+''' This program enables users to create a country mod for democracy 3 '''
+
+import tkinter as tk
+
+# ***** GLOBAL CONSTANTS *****
+BG_COLOUR = "white"
+
+# ***** CREATE WINDOW *****
+root = tk.Tk()
+root.title("Democracy 3 Country Mod Maker")
+root.config(bg=BG_COLOUR)
+
+
+# ***** ADD CONTENT TO WINDOW *****
+
+# ***** Header
+
+
+# ***** RUN WINDOW LOOP *****
+root.mainloop()
